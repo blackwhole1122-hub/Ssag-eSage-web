436195d0e891c600f330081774a36fac4c260299
blackwhole1122-hub <blackwhole1122@gmail.com>
2026-03-28 21:02:26 +0900

    키워드 매칭

--- src/lib/keywords.js
+++ src/lib/keywords.js
@@ -1,49 +1,68 @@
 export const KEYWORD_GROUPS = {
   "식품": [
-    // --- 기존 데이터 유지 ---
-    { group: "햇반", slug: "hatban", keywords: ["햇반 210"] },
-    { group: "오뚜기밥", slug: "ottogi-rice", keywords: ["오뚜기밥 210"] },
-    { group: "스팸 클래식", slug: "spam-classic", keywords: ["스팸 클래식 200", "스팸 클래식 340"] },
-    { group: "비비고 왕교자", slug: "bibigo-mandu", keywords: ["비비고 왕교자"] },
-    { group: "비비고 사골곰탕", slug: "bibigo-sogalcomtang", keywords: ["비비고 사골곰탕"] },
-    { group: "조선호텔 김치", slug: "josunhotel-kimchi", keywords: ["조선호텔"] },
-    { group: "동원참치", slug: "dongwon-tuna", keywords: ["동원참치"] },
-    { group: "동원비엔나", slug: "dongwon-vienna", keywords: ["동원비엔나"] },
-    { group: "유혜광 돈까스", slug: "yuhyekwang", keywords: ["유혜광"] },
-    { group: "쟌슨빌 소시지", slug: "johnsonville", keywords: ["쟌슨빌"] },
-    
-    // --- 신규 쌀/잡곡 추가 ---
-    { group: "김포금 쌀", slug: "gimpogeum-rice", keywords: ["김포금"] },
-    { group: "새청무 쌀", slug: "saecheongmu-rice", keywords: ["새청무"] },
-    { group: "수향미", slug: "suhyang-rice", keywords: ["수향미"] },
+    // --- 가공식품 ---
+    { group: "햇반 210g", slug: "cj-hatbahn-210g", keywords: ["햇반 210", "햇반"] },
+    { group: "오뚜기밥 210g", slug: "otogirice-210g", keywords: ["오뚜기밥 210", "오뚜기밥"] },
+    { group: "스팸클래식 200g", slug: "spamclassic-200g", keywords: ["스팸 클래식 200", "스팸클래식 200"] },
+    { group: "스팸클래식 340g", slug: "spamclassic-340g", keywords: ["스팸 클래식 340", "스팸클래식 340"] },
+    { group: "비비고 왕교자 1.05kg", slug: "bibigo-mandu-1050g", keywords: ["비비고 왕교자 1.05", "비비고 왕교자 1050", "왕교자 1.05", "왕교자 1050"] },
+    { group: "비비고 왕교자 455g", slug: "bibigo-mandu-455g", keywords: ["비비고 왕교자 455", "왕교자 455"] },
+    { group: "비비고 사골곰탕 500g", slug: "bibigo-beef-boon-soup-500g", keywords: ["비비고 사골곰탕 500", "사골곰탕 500", "비비고 곰탕 500"] },
+    { group: "비비고 사골곰탕 300g", slug: "bibigo-beef-boon-soup-300g", keywords: ["비비고 사골곰탕 300", "비비고 사골 300"] },
+    { group: "동원 참치 라이트 150g", slug: "dongwon-right-tuna-150g", keywords: ["동원참치 150", "동원참치 라이트", "라이트스탠다드"] },
+    { group: "동원 통그릴 비엔나 1kg", slug: "dongwon-vienna-1kg", keywords: ["동원비엔나 1kg", "동원 비엔나 1kg", "통그릴 비엔나", "동원비엔나"] },
+    { group: "유혜광돈까스", slug: "yhgfood", keywords: ["유혜광", "유혜광돈까스"] },
+    { group: "쟌슨빌 소시지", slug: "johnsonville", keywords: ["쟌슨빌", "쟌슨빌 소시지"] },
+    { group: "오뚜기 프레스코 600g", slug: "otogi-fresco-600g", keywords: ["오뚜기프레스코", "프레스코", "오뚜기 소스"] },
+    { group: "폰타나파스타소스 600g", slug: "fontana-pasta-sauce-600g", keywords: ["폰타나 파스타", "폰타나"] },
+    { group: "데체코 올리브오일 1l", slug: "dececco-olive-oil-1l", keywords: ["데체코", "데체코 올리브"] },
 
-    // --- 음료/워터류 ---
-    { group: "펩시 제로", slug: "pepsi-zero", keywords: ["펩시 제로 210", "펩시 제로 245", "펩시 제로 355", "펩시 제로 500"] },
-    { group: "칠성사이다 제로", slug: "chilsung-zero", keywords: ["칠성 제로"] },
+    // --- 신선식품 ---
+    { group: "조선호텔 김치 8kg", slug: "josunkimchi-8kg", keywords: ["조선호텔 8kg", "조선호텔 김치 8", "조선김치 8"] },
+    { group: "조선호텔 김치 4kg", slug: "josunkimchi-4kg", keywords: ["조선호텔 4kg", "조선호텔 김치 4", "조선김치 4"] },
+    { group: "조선호텔 김치 2.5kg", slug: "josunkimchi-2.5kg", keywords: ["조선호텔 2.5kg", "조선호텔 김치 2.5", "조선김치 2.5"] },
+    { group: "스테비아 토마토 2kg", slug: "stevia-tomato-2kg", keywords: ["스테비아 토마토", "토망고"] },
+
+    // --- 쌀/잡곡 ---
+    { group: "김포금쌀 고시히카리 10kg", slug: "gimpo-rice-10kg", keywords: ["김포금 10", "김포금쌀", "고시히카리"] },
+    { group: "새청무 쌀 10kg", slug: "saecheongmu-rice-10kg", keywords: ["새청무 10", "새청무 쌀"] },
+    { group: "수향미 10kg", slug: "suhyangmi-rice-10kg", keywords: ["수향미 10", "수향미"] },
+
+    // --- 음료/탄산 ---
+    { group: "펩시 제로 210ml", slug: "pepsi-zero-210ml", keywords: ["펩시 제로 210"] },
+    { group: "펩시 제로 245ml", slug: "pepsi-zero-245ml", keywords: ["펩시 제로 245"] },
+    { group: "펩시 제로 355ml", slug: "pepsi-zero-355ml", keywords: ["펩시 제로 355"] },
+    { group: "펩시 제로 500ml", slug: "pepsi-zero-500ml", keywords: ["펩시 제로 500", "펩시제로 500"] },
+    { group: "칠성사이다제로 210ml", slug: "chilsung-cider-zero-210ml", keywords: ["칠성 제로", "칠성사이다 제로", "칠성제로"] },
+
+    // --- 생수 ---
     { group: "삼다수 500ml", slug: "samdasoo-500ml", keywords: ["삼다수 500"] },
-    { group: "삼다수 2L", slug: "samdasoo-2l", keywords: ["삼다수 2"] },
-    { group: "초록매실 제로", slug: "green-plum-zero", keywords: ["초록매실 제로"] },
-    { group: "몬스터 에너지", slug: "monster-energy", keywords: ["몬스터 에너지"] },
+    { group: "삼다수 2l", slug: "samdasoo-2l", keywords: ["삼다수 2"] },
 
-    // --- 식재료/간편식 ---
-    { group: "데체코 올리브오일", slug: "dececco-oil", keywords: ["데체코"] },
-    { group: "스테비아 토마토", slug: "stevia-tomato", keywords: ["스테비아 토마토"] },
-    { group: "오뚜기 프레스코", slug: "ottogi-fresco", keywords: ["오뚜기프레스코"] },
-    { group: "폰타나 파스타", slug: "fontana-pasta", keywords: ["폰타나 파스타"] },
-    
-    // --- 디저트 및 영양제 ---
-    { group: "벤앤제리스", slug: "ben-and-jerrys", keywords: ["벤앤제리스", "벤엔제리스"] },
-    { group: "하겐다즈", slug: "haagen-dazs", keywords: ["하겐다즈"] },
-    { group: "스포츠리서치 오메가3", slug: "omega3", keywords: ["스포츠리서치", "오메가3"] },
+    // --- 음료 ---
+    { group: "초록매실 제로 1.5l", slug: "green-plum-1.5l", keywords: ["초록매실 제로 1.5", "초록매실 1.5"] },
+    { group: "초록매실 제로 500ml", slug: "green-plum-500ml", keywords: ["초록매실 제로 500", "초록매실 500"] },
+
+    // --- 에너지음료 ---
+    { group: "몬스터 에너지", slug: "monster-energy-355g", keywords: ["몬스터 에너지", "몬스터", "몬스터에너지"] },
+
+    // --- 디저트/아이스크림 ---
+    { group: "벤앤제리스 파인트", slug: "benjerry", keywords: ["벤앤제리스", "벤엔제리스", "밴앤제리스"] },
+    { group: "하겐다즈파인트", slug: "haagendazs-473g", keywords: ["하겐다즈", "하겐"] },
+
+    // --- 영양제 ---
+    { group: "스포츠 리서치 오메가", slug: "sports-research-omega3", keywords: ["스포츠리서치", "오메가3"] },
   ],
   "생활잡화": [
-    { group: "크리넥스 3겹", slug: "kleenex-3ply", keywords: ["크리넥스 3겹", "데코앤소프트"] },
-    { group: "다우니", slug: "downy", keywords: ["다우니"] },
+    { group: "크리넥스 3겹 30m", slug: "kleenex-tissue-30m", keywords: ["크리넥스 3겹 30", "크리넥스 30m"] },
+    { group: "크리넥스 3겹 25m", slug: "kleenex-tissue-25m", keywords: ["크리넥스 3겹 25", "크리넥스 25m", "데코앤소프트"] },
+    { group: "다우니 1l", slug: "downy-1l", keywords: ["다우니", "다우니 1L"] },
   ],
   "가전/디지털": [
-    { group: "PS5 Pro", slug: "ps5-pro", keywords: ["PS5 Pro", "플스5 프로", "ps5 pro"] },
+    { group: "ps5pro", slug: "ps5pro", keywords: ["PS5 Pro", "플스5 프로", "ps5 pro", "ps5pro", "플스5프로"] },
   ],
   "상품권": [
-    { group: "컬쳐랜드", slug: "cultureland", keywords: ["컬쳐랜드"] },
+    { group: "컬쳐랜드 문화상품권 1만원", slug: "cultureland-10k", keywords: ["컬쳐랜드 1만", "컬쳐 1만"] },
+    { group: "컬쳐랜드 문화상품권 5만원", slug: "cultureland-50k", keywords: ["컬쳐랜드 5만", "컬쳐 5만"] },
   ],
 };
